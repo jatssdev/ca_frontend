6021dd70912b44e5b1a9a3ea48772b1e9b1319a8
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-06-27 14:01:23 +0530

    i think its last time

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/front': {
-  //       target: 'https://account.meracacs.com/',
-  //       // target: 'http://localhost/ca',
-  //       changeOrigin: true,
-  //       secure: true
-  //     }
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/front': {
+        target: 'https://account.meracacs.com/',
+        // target: 'http://localhost/ca',
+        changeOrigin: true,
+        secure: true
+      }
+    },
+  },
   plugins: [react()],
 })
